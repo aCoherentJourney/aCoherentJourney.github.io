63f5eee8012a4fc0cd35fbcbb1c689a8b0c6cc3a
Dennis Fink <moglicodes@gmail.com>
2018-11-15 14:30:45 +0100

    Sass structure

--- docs/js/app.js
+++ docs/js/app.js
@@ -2,6 +2,6 @@ console.log("Hello from app.js")
 
 
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('start', '_assets/js/particles.json', function() {
+particlesJS.load('start', './docs/js/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
